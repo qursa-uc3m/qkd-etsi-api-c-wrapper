e9ef5c2af6b5d3c52a3db5720d15894add87d603
fj-blanco <fjavib@gmail.com>
2025-03-24 09:41:48 +0100

    integrating qubip etsi 004

---
+++ include/etsi004/backends/python_client.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2024 QURSA Project
+ * SPDX-License-Identifier: MIT
+ *
+ * Authors:
+ * - Javier Blanco-Romero (@fj-blanco) - UC3M
+ */
+
+/*
+ * include/etsi004/backends/python_client.h
+ */
+
+ #ifndef QKD_ETSI004_BACKENDS_PYTHON_CLIENT_H_
+ #define QKD_ETSI004_BACKENDS_PYTHON_CLIENT_H_
+ 
+ #include "etsi004/api.h"
+ 
+ extern const struct qkd_004_backend python_client_backend;
+ 
+ #endif /* QKD_ETSI004_BACKENDS_PYTHON_CLIENT_H_ */
